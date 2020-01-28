0bb65167b2fd749716847c8402e3efcca1cd96f7
RKauf00 <rkauf00@outlook.com>
2020-01-28 07:56:38 -0500

    Update cleanup and deploy.ps1

--- scripts/cleanup and deploy.ps1
+++ scripts/cleanup and deploy.ps1
@@ -1,41 +1,68 @@
-﻿$tenantID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-$subscriptionID = "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy"
 
-Connect-AzureRmAccount -TenantId $tenantID -Subscription $subscriptionID
+    # Params﻿
 
-# create administrator password secure string object
-$SecureAdminPassword = Read-Host -AsSecureString -Prompt "Provide local Administrator password for Azure Stack host VM" | ConvertTo-SecureString -AsPlainText -Force
-#$SecureAdminPassword = "SuperSecurePassword123!!" | ConvertTo-SecureString -AsPlainText -Force
+        $tenantID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+        $subscriptionID = "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy"
 
-# declare variables 
-[int]$instanceNumber = 1 # resource group name will be generated based on this number. 
-[bool]$autoDownloadASDK = $false #either download latest ADSK in the VM or not. Setting this to $true will add additional ~35 mins to ARM template deployment time.
-[string]$resourceGroupNamePrefix = "yagmursasdk" #Resource group name will be generated based on this prefix. Ex. yagmursasdk-1
-[string]$publicDnsNamePrefix = "yagmursasdkinstance" # This will will be concatenated with $instancenumber. Ex. yagmursasdkinstance1.eastus2.cloudapp.azure.com
-[string]$location = 'East US2' # can be any region that supports E and D VM sizes that supports nested virtualization.
-[string]$virtualMachineSize = "Standard_E32s_v3" # 1811 and upper versions require 256GB RAM
-[ValidateSet("development", "master")][string]$gitBranch = "master" # github branch 
-[string]$resourceGroupName = "$resourceGroupNamePrefix-$instanceNumber"
-[string]$publicDnsName = "$publicDnsNamePrefix$instanceNumber"
+        $DeploymentTest = $TRUE                                                   # $TRUE deletes resource group post deployment
+        [string]$AzEnv = 'AzureUSGovernment'
+        [string]$Location = 'usgovtexas'                                          # 'usgovtexas' 'East US2'  ||  Region must host E and D VM sizes supporting nested virtualization
+        
+    # Connect Azure Account
 
-# create ARM template parameter object
-$templateParameterObject = @{}
-$templateParameterObject.Add("adminPassword", $SecureAdminPassword)
-$templateParameterObject.Add("publicDnsName",$publicDnsName.ToLower())
-$templateParameterObject.Add("autoDownloadASDK", $autoDownloadASDK)
-$templateParameterObject.Add("virtualMachineSize", $virtualMachineSize)
+        if ($AzEnv)
+        {
+            Connect-AzAccount -TenantId $tenantID -Subscription $subscriptionID -Environment $AzEnv
+            [ValidateSet("usgovvirginia","usgoviowa","usdodeast","usdodcentral","usgovtexas","usgovarizona")][string]$location = $Location
+        }
+        else
+        {
+            Connect-AzAccount -TenantId $tenantID -Subscription $subscriptionID
+            [ValidateSet("eastasia","southeastasia","centralus","eastus","eastus2","westus","northcentralus","southcentralus","northeurope","westeurope","japanwest","japaneast","bazilsouth","australiaeast","australiasoutheast","southindia","centralindia","westindia","canadacentral","canadaeast","uksouth","ukwest","westcentralus","wstus2","koreacentral","koreasouth","francecentral","francesouth","australiacentral","australiacentral2","uaecentral","uaenorth","southafricanorth","southaricawest","switzerlandnorth","switzerlandwest","germanynorth","germanywestcentral","norwaywest","norwayeast")][string]$location = $Location
+        }
 
-# create resource group
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+    # Set Administrator Password
+    
+        $SecureAdminPassword = Read-Host -AsSecureString -Prompt "Provide local Administrator password for Azure Stack host VM" | ConvertTo-SecureString -AsPlainText -Force
+    
+    # Variables
 
-# deploy ARM template from github using locally provided ARM template parameters
-New-AzureRmResourceGroupDeployment -Name "$resourceGroupName-PoC-Deployment" -ResourceGroupName $resourceGroupName `
-    -TemplateUri "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/$gitBranch/azuredeploy.json" `
-    -TemplateParameterObject $templateParameterObject `
-    -Mode Incremental `
-    -AsJob
+        [int]$instanceNumber = 1                                                  # Resource Group Name Suffix
+        [bool]$autoDownloadASDK = $TRUE                                           # $TRUE or $FALSE; $TRUE adds ~35 mins to deployment time
+        [string]$resourceGroupNamePrefix = "AzStackPOC"                           # Resource Group Name Prefix
+        [string]$publicDnsNamePrefix = "AzStackPOC"                               # DNS Name Prefix
+        [string]$virtualMachineSize = "Standard_E32s_v3"                          # v1811+ requires 256GB RAM
+        [ValidateSet("development","master")][string]$gitBranch = "master"        # GitHub branch 
+        [string]$resourceGroupName = "$resourceGroupNamePrefix-$instanceNumber"
+        [string]$publicDnsName = "$publicDnsNamePrefix$instanceNumber"
+        #[string]$location = 'East US2'                                            # can be any region that supports E and D VM sizes that supports nested virtualization.
 
+    # Create ARM template parameter object
 
-Pause
-# delete resource group and all object in the RG
-Get-AzureRmResourceGroup -Name $resourceGroupName | Remove-AzureRmResourceGroup -AsJob #-Force
+        $templateParameterObject = @{}
+        $templateParameterObject.Add("adminPassword", $SecureAdminPassword)
+        $templateParameterObject.Add("publicDnsName",$publicDnsName.ToLower())
+        $templateParameterObject.Add("autoDownloadASDK", $autoDownloadASDK)
+        $templateParameterObject.Add("virtualMachineSize", $virtualMachineSize)
+
+    # Create Resource Group
+
+        New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+    # Deploy GitHub ARM template using local ARM template parameters
+
+        New-AzResourceGroupDeployment `
+            -Name "$resourceGroupName-POC-Deployment" `
+            -ResourceGroupName $resourceGroupName `
+            -TemplateUri "https://raw.GitHubusercontent.com/RKauf00/AzureStack-VM-PoC/$gitBranch/azuredeploy.json" `
+            -TemplateParameterObject $templateParameterObject `
+            -Mode Incremental `
+            -AsJob
+
+    # Purge Resource Group if DeploymentTest set to TRUE
+
+        if ($DeploymentTest -eq $TRUE)
+        {
+            Pause
+            Get-AzResourceGroup -Name $resourceGroupName | Remove-AzResourceGroup -AsJob -Force
+        }
