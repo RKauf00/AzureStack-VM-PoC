2a048f666c8c8f2a4b759ea7cdb0430771cfa6e0
Rick <rkauf00@outlook.com>
2020-02-27 14:56:22 -0500

    file add

--- scripts/New-AzStackARMDeployment.ps1
+++ scripts/New-AzStackARMDeployment.ps1
@@ -110,7 +110,7 @@
 # Template Variables
 
     # Set Instance Number
-    [int]    $instanceNumber           =  3                                      # Resource Group Name Suffix
+    [int]    $instanceNumber           =  1                                      # Resource Group Name Suffix
 
     # Set Azure Values
  
