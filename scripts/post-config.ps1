be9664c834d0a2db263706bc094d773dbfddc265
Rick <rkauf00@outlook.com>
2020-03-30 16:21:04 -0400

    download uri update

--- scripts/post-config.ps1
+++ scripts/post-config.ps1
@@ -189,7 +189,7 @@
             # Download ISO Files
             if ($ASDKConfiguratorParams.IsoPath2019)
             {
-                DownloadWithRetry -Uri https://software-download.microsoft.com/download/pr/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso -DownloadLocation $ASDKConfiguratorParams.IsoPath2019
+                DownloadWithRetry -Uri https://software-download.microsoft.com/download/en-us/17763.253.190108-0006.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso -DownloadLocation $ASDKConfiguratorParams.IsoPath2019
             }
 
             if ($ASDKConfiguratorParams.IsoPath)
